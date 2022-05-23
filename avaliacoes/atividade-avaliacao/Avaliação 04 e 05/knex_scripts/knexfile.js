02d57dc77a56161e1f34764b056a7ba3f9441895
rbqueiroz13 <102779100+rbqueiroz13@users.noreply.github.com>
2022-05-23 15:44:00 -0300

    Alterado em 23-05-2022

--- avaliacoes/atividade-avaliacao/Avaliação 04 e 05/knex_scripts/knexfile.js
+++ avaliacoes/atividade-avaliacao/Avaliação 04 e 05/knex_scripts/knexfile.js
@@ -8,7 +8,10 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './database/av05.db'
+    },
+    seeds: {
+      directory: './seeds'
     }
   },
 
